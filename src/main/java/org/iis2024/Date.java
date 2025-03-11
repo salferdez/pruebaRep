d4a444a042da25526d99841a1a96a089318be555
Pablo Castellano <pablocastellanojkl@gmail.com>
2025-03-11 09:50:47 +0100

    Método modificado

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -52,12 +52,12 @@ public class Date {
     return year;
   }
 
-  // Constructor modified by the owner
-  public Date(int dia, int mes, int año) {
-	this.day = dia;
-	this.month = mes;
-	this.year = año;
-}
+  // Constructor modified by the collaborator
+  public Date(int tag, int monat, int jahr) {
+    this.day = tag;
+    this.month = monat;
+    this.year = jahr;
+  }
 
   /**
    * Validates the date according to the predefined rules: the year must be within 1900 to 2050, the
