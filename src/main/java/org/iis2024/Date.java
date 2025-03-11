8aec8c6391cdf4fd3934b8bfe24958fa0814b915
salferdez <salferdez@gmail.com>
2025-03-11 09:54:49 +0100

    Modified constructor (owner)

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -52,11 +52,11 @@ public class Date {
     return year;
   }
 
-  // Constructor modified by the collaborator
-  public Date(int tag, int monat, int jahr) {
-    this.day = tag;
-    this.month = monat;
-    this.year = jahr;
+  // Constructor modified by the owner
+  public Date(int dia, int mes, int año) {
+	this.day = dia;
+	this.month = mes;
+	this.year = año;
   }
 
   /**
